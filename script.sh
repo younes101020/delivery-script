d5e80df54ce3e3c37cfb8bae6ecb6221abf2f42d
younes101020 <senyoupl@icloud.com>
2026-03-09 01:08:16 +0100

    chore: trigger s3 sync

--- script.sh
+++ script.sh
@@ -353,11 +353,11 @@ else
 fi
 
 echo -e "5. Download required files from CDN. "
-curl -fsSL http://deliveryinstallresources.younesfakallah.com/compose.prod.yaml -o /data/delivery/source/compose.prod.yaml
+curl -fsSL https://deliveryinstallresources.younesfakallah.com/compose.prod.yaml -o /data/delivery/source/compose.prod.yaml
 #curl -fsSL https://raw.githubusercontent.com/younes101020/delivery/refs/heads/main/infrastructure/traefik-config/acme.json -o /data/delivery/source/infrastructure/traefik-config/acme.json || true
-curl -fsSL http://deliveryinstallresources.younesfakallah.com/dynamic.yaml -o /data/delivery/source/infrastructure/traefik-config/dynamic.yaml
-curl -fsSL http://deliveryinstallresources.younesfakallah.com/traefik.yaml -o /data/delivery/source/infrastructure/traefik-config/traefik.yaml
-curl -fsSL http://deliveryinstallresources.younesfakallah.com/env.production -o /data/delivery/source/.env.production
+curl -fsSL https://deliveryinstallresources.younesfakallah.com/dynamic.yaml -o /data/delivery/source/infrastructure/traefik-config/dynamic.yaml
+curl -fsSL https://deliveryinstallresources.younesfakallah.com/traefik.yaml -o /data/delivery/source/infrastructure/traefik-config/traefik.yaml
+curl -fsSL https://deliveryinstallresources.younesfakallah.com/env.production -o /data/delivery/source/.env.production
 
 touch /data/delivery/source/infrastructure/traefik-config/acme.json
 chmod 600 /data/delivery/source/infrastructure/traefik-config/acme.json
